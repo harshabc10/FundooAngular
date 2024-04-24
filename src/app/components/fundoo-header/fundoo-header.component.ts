9dcb6d9dd52a2ab28b3e537b1b4fcf997ab19656
harshabc10 <harshabc10@gmail.com>
2024-04-24 15:52:51 +0530

    implemented note card deign and take note design given routes to the main container

--- src/app/components/fundoo-header/fundoo-header.component.ts
+++ src/app/components/fundoo-header/fundoo-header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { LIST_VIEW_ICON, MENU_ICON, OTHER_MENU_ICON, REFRESH_ICON, SEARCH_ICON, SETTING_ICON } from 'src/assets/svg-icons';
 
 @Component({
   selector: 'app-fundoo-header',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FundooHeaderComponent implements OnInit {
 
-  constructor() { }
+  isDrawerOpen = false;
+
+  constructor(private domSanitizer:DomSanitizer,private matIconRegistry:MatIconRegistry) { 
+    matIconRegistry.addSvgIconLiteral("menu-icon", domSanitizer.bypassSecurityTrustHtml(MENU_ICON)),
+    matIconRegistry.addSvgIconLiteral("search-icon", domSanitizer.bypassSecurityTrustHtml(SEARCH_ICON)),
+    matIconRegistry.addSvgIconLiteral("refresh-icon", domSanitizer.bypassSecurityTrustHtml(REFRESH_ICON)),
+    matIconRegistry.addSvgIconLiteral("setting-icon", domSanitizer.bypassSecurityTrustHtml(SETTING_ICON)),
+    matIconRegistry.addSvgIconLiteral("list-view-icon", domSanitizer.bypassSecurityTrustHtml(LIST_VIEW_ICON))
+    matIconRegistry.addSvgIconLiteral("other-menu-icon", domSanitizer.bypassSecurityTrustHtml(OTHER_MENU_ICON))
+   }
 
   ngOnInit(): void {
   }
 
+  toggleDrawer() {
+    console.log("toggle drawer");
+    this.isDrawerOpen = !this.isDrawerOpen;
+  }
 }
--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ARCHIVE_ICON, EDIT_ICON, NOTE_ICON, REMINDER_ICON, TRASH_ICON } from 'src/assets/svg-icons';
 
 @Component({
   selector: 'app-side-nav',
@@ -6,8 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
+  isDrawerOpen = false;
 
-  constructor() { }
+  constructor(private domSanitizer:DomSanitizer,private matIconRegistry:MatIconRegistry) {
+    matIconRegistry.addSvgIconLiteral("note-icon", domSanitizer.bypassSecurityTrustHtml(NOTE_ICON)),
+    matIconRegistry.addSvgIconLiteral("reminder-icon", domSanitizer.bypassSecurityTrustHtml(REMINDER_ICON)),
+    matIconRegistry.addSvgIconLiteral("edit-icon", domSanitizer.bypassSecurityTrustHtml(EDIT_ICON)),
+    matIconRegistry.addSvgIconLiteral("archive-icon", domSanitizer.bypassSecurityTrustHtml(ARCHIVE_ICON)),
+    matIconRegistry.addSvgIconLiteral("trash-icon", domSanitizer.bypassSecurityTrustHtml(TRASH_ICON))
+   }
 
   ngOnInit(): void {
   }
